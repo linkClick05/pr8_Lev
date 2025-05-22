6d601314b1b5a2127d9119aa892e39af9df406ce
Name <you@example.com>
2025-05-22 06:14:16 +0300

    Mодернизироваkb проект pasword1

--- pasword1/src/Main.java
+++ pasword1/src/Main.java
@@ -3,20 +3,20 @@ import java.util.Random;
 public class Main {
 
     public static String generatePassword(int length) {
-        String lowercase = "abcdefghijklmnopqrstuvwxyz";
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random random = new Random();
         StringBuilder password = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
-            int index = random.nextInt(lowercase.length());
-            password.append(lowercase.charAt(index));
+            int index = random.nextInt(alphabet.length());
+            password.append(alphabet.charAt(index));
         }
 
         return password.toString();
     }
 
     public static void main(String[] args) {
-        int length = 10; 
+        int length = 10;
         System.out.println("Сгенерированный пароль: " + generatePassword(length));
     }
 }
